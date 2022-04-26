13e4fafdcc1af88d378611fd8d1ed86479ddd364
Mark Bucholski <mbucholski95@gmail.com>
2022-04-26 17:11:58 -0400

    Removed unnecessary comments in JavaScript file

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-//STEP 2 - CUSTOMIZABLE QUOTES PRINTED TO HTML USING THE DOM
 
-// create a bookQuotes array with two array items, the first being quotes from 1984 and the second quotes from Animal Farm.
-// Within each book array add another inner array with three array items. The first array item will be for the first part of the sentence, the second array item for the middle part of the sentence, and the third array item for the ending part of the sentence.
+
+//  A bookQuotes array with two array items, the first being quotes from 1984 and the second quotes from Animal Farm.  Each book array contains three array items pertaining to the beginning, middle and end of a quote.
 const bookQuotes = [
     [
         ['"Not merely the validity of experience', '"What can you do,', '"Asleep or awake,', '"There was truth and there was untruth,', '"Your worst enemy,', '"We know that no one', '"Power is in tearing human minds to pieces', '"Nothing exists', '"Who controls the past', '"We shall squeeze you empty,', '"Always there will be', '"To the future or the past,', '"For the first time he perceived', '"The object to waging a war', '"At present nothing is possible'],
@@ -19,15 +18,7 @@ const bookQuotes = [
 const radio1984 = document.getElementById('1984_book');
 const radioAnimalFarm = document.getElementById('animal_farm_book');
 
-// Create an orwellianQuoteGenerator function which will randomly generate a quote from either 1984 or animal farm from the two arrays holding respective sentence fragments.
-// In the orwellianQuoteGenerator function first generate a random number using a random number function between 1 and the number of fragment quotes each sentence fragment array item holds.
-// Establish a conditional if statement where the condition is if the select radio control value from either 1984 or Animal Farm is checked will be determined in the condition by applying the checked property to both radio controls for 1984 and animal farm. 
-// Whichever radio control is checked will then cause that condition to equal true causing the code associated with that book which has the radio checked evalute to true in the condition be executed.
-// If the 1984 radio control is checked then three sentences are generated from the 1984 array using three variables which take random sentence parts chosen by making the index number from the Animal Farm's array's array the value of the random number function. 
-// If the Animal Farm radio control is checked then three sentences are generated from the Animal Farm array using three variables which take random sentence parts chosen by making the index number the Animal Farm's array's array the value of the random number function. 
-// After the condition make the value the orwellianQuotationGenerator returns the three string variable concated inorder of appearance, in order to return a string with three random parts of quotes from either 1984 or Animal Farm.
-// If neither of the radio controlls are selected make the return value of the orwellianQuoteGenerator function be an alert telling the user to select one of the radio controls in order to return quotes from either 1984 or Animal Farm.
-
+// An orwellianQuoteGenerator function will randomly generate a quote from either 1984 or animal farm from the two arrays holding respective sentence fragments.
 const orwellianQuoteGenerator = (bookQuotes) => {
     const randomNumber = () => Math.floor(Math.random() * 15);
     if ( radio1984.checked === true ) {
@@ -43,15 +34,7 @@ const orwellianQuoteGenerator = (bookQuotes) => {
     } 
 }
 
-// Create a function which displays the number of quotes based on the number of quotes a user selected
-// In the function first select the div the quotes will be printed to using the DOM and set the div's initial value to empty quotes so each time the function is run the existing quotes are cleared to make way for new quotes.
-// Create a numberOfQuotes variable to hold the value of the quotationNumber select menu.
-// Establish a for loop which iterates the same number of times as the value of the numberOfQuotes variable.
-// For each iteration in the for loop print a quote onto the div intended to have the quotes printed onto it.
-// The quotes print will be returned as the value for the orwellianQuoteGenerator function.
-// Thus, the quote printed from the orwellianQuoteGenerator function followed by a <br> element will be continously added to the quote div through each iteration of the for loop.
-// Use a clickon attribute in html for the generate quotes button element to print the quotes from the generateMultipleQuotes function by make the value of the generate quote's button's clickon attribute the calling of the generateMultipleQuotes function.
-
+// A function which displays the number of quotes based on the number of quotes a user selected
 const generateMultipleQuotes = () => {
     document.getElementById('printedQuotes').innerHTML = '';
     const numberOfQuotes = document.getElementById('quotationNumber').value;
@@ -69,9 +52,7 @@ const generateMultipleQuotes = () => {
     }
 }
 
-// Create a function which will cause the quotes to disappear once the clear button is clicked.
-// Use a clickon attribute in html for the clear button element to clear any quotes by making the calling of the clearQuotes function the value for the clickon attribute of the clear quotes button.
-
+// A function which will cause the quotes to disappear once the clear button is clicked.
 const clearQuotes = () => document.getElementById('printedQuotes').innerHTML = '';
 
 
